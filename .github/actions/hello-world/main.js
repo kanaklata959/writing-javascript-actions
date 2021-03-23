e0e39addc92c4178714a731446bfd3b55fe49655
Kanklata Kumari <kanklata.kumari@tcs.com>
2021-03-23 12:41:02 +0200

    allow input in all action files

--- .github/actions/hello-world/main.js
+++ .github/actions/hello-world/main.js
@@ -1,3 +1,15 @@
-runs:
-  using: "node12"
-  main: "bread.js"
+const core = require("@actions/core");
+
+const firstGreeting = core.getInput("first-greeting");
+const secondGreeting = core.getInput("second-greeting");
+const thirdGreeting = core.getInput("third-greeting");
+
+console.log(`Hello ${firstGreeting}`);
+console.log(`Hello ${secondGreeting}`);
+if (thirdGreeting) {
+  console.log(`Hello ${thirdGreeting}`);
+}
+
+
+
+
